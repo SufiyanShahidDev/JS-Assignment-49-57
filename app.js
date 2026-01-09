fb7c4892bed9c06273eb4d51aa65ff9c3814ddfb
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2026-01-09 20:57:38 +0500

    Initial commit

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 
+// function seeMore (e){
+//     paragragh.innerText="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo enim quae facilis! Magni?Lorem ipsum dolor sit amet consectetur. Nobis ipsum tempora at. Quos, minima ab."
+//     btn.innerText="See Less"
+
+// }
+
+// function seeLess (e){
+//     paragragh.innerText="Lorem ipsum dolor sit amet."
+//     btn.innerText="See More"
+// }
+
+// function seeBtn(){
+//     if (btn.innerText == "See More"){
+//         return seeMore(this);
+
+//     }else{
+//         return seeLess(this)
+//     }
+// }
